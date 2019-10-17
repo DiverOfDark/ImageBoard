e25aafa667a982bb02d73a62d931424f27e303f3
Kirill Orlov <i@kirillorlov.pro>
2019-10-18 01:52:26 +0300

    fixed listening port

--- src/ImageBoard/Program.cs
+++ src/ImageBoard/Program.cs
@@ -18,6 +18,8 @@ namespace ImageBoard
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://0.0.0.0:80/")
+                );
     }
 }
